924dc2fcc16a19f4ea13011a2f2769d5d0595d78
edu <reuben.omanyala22@gmail.com>
2020-06-02 20:05:24 +0300

    Added IP

--- CPA/CPA/settings.py
+++ CPA/CPA/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'CPA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
     #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'blogdb',
-       # 'USER': 'manager',
-        #'PASSWORD': 'avails7442ffGh3636',
-       # 'HOST': 'localhost',
-      #  'PORT': '',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'manager',
+        'PASSWORD': 'avails7442ffGh3636',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
