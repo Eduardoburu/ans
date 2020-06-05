aebabea3e9f33e4a63db7afe1f926fa493f00671
edu <reuben.omanyala22@gmail.com>
2020-06-05 21:41:49 +0300

    Added 2018 FA Nov Question paper

--- CPA/financialAccounting/urls.py
+++ CPA/financialAccounting/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Fa2019_novView, Fa_listView
+from .views import Fa2019_novView, Fa_listView, Fa2018_novView
 from . import views
 
 
 
 urlpatterns = [
     path('2019_November', Fa2019_novView.as_view(), name='financial_accounting'),
+    path('2018_November', Fa2018_novView.as_view(), name='2018_FA_Nov'),
     path('fa_questions_list', Fa_listView.as_view(), name='fa_questions_list'),
 ]
--- CPA/financialAccounting/views.py
+++ CPA/financialAccounting/views.py
@@ -15,4 +15,4 @@ class Fa2019_novView(View):
 
 class Fa2018_novView(View):
     def get(self, *args, **kwargs):
-        return render(self.request, "financialAccounting/2018_fa_one.html")
+        return render(self.request, "financialAccounting/2018_fa_Nov.html")
